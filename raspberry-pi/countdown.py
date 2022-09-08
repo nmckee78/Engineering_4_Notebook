5d1a12c98c6c2a8fc4f8a2f23cdea09e0f8b8ccf
nmckee78 <71345228+nmckee78@users.noreply.github.com>
2022-09-08 13:36:28 -0400

    Working code

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -2,9 +2,9 @@
 import board
 import time
  
-for x in range(10,0,-1):
+for x in range(10,-1,-1):
     if x == 0:
         print("Liftoff!")
     else: 
         print(x)
-        time.sleep(.1)
+        time.sleep(1)
