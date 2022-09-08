ecaf1780fef42245ee5b7aba04fc5517a1be70ca
nmckee78 <71345228+nmckee78@users.noreply.github.com>
2022-09-08 14:10:17 -0400

    Update countdown.py

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -1,10 +1,24 @@
-
 import board
 import time
- 
+import digitalio
+
+led1 = digitalio.DigitalInOut(board.GP13)
+led2 = digitalio.DigitalInOut(board.GP18)
+led1.direction = digitalio.Direction.OUTPUT
+led2.direction = digitalio.Direction.OUTPUT
+
 for x in range(10,-1,-1):
     if x == 0:
         print("Liftoff!")
+        led1.value = True
+        time.sleep(1)
     else: 
         print(x)
         time.sleep(1)
+        led2.value = True
+        time.sleep(.2)
+        led2.value = False
+        time.sleep(.2)
+
+while True:
+    pass
