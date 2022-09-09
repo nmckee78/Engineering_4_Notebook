277a5650b6c916ba029ec9904dcb6138f36b1a80
nmckee78 <71345228+nmckee78@users.noreply.github.com>
2022-09-09 14:11:05 -0400

    Commit

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -1,3 +1,4 @@
+
 import board
 import time
 import digitalio
@@ -6,19 +7,21 @@ led1 = digitalio.DigitalInOut(board.GP13)
 led2 = digitalio.DigitalInOut(board.GP18)
 led1.direction = digitalio.Direction.OUTPUT
 led2.direction = digitalio.Direction.OUTPUT
-
-for x in range(10,-1,-1):
-    if x == 0:
-        print("Liftoff!")
-        led1.value = True
-        time.sleep(1)
-    else: 
-        print(x)
-        time.sleep(1)
-        led2.value = True
-        time.sleep(.2)
-        led2.value = False
-        time.sleep(.2)
+button = digitalio.DigitalInOut(board.GP22)
+button.pull = digitalio.Pull.UP
 
 while True:
-    pass
+    if button.value == False:
+     for x in range(10,-1,-1):
+        if x == 0:
+         print("Liftoff!")
+         led1.value = True
+         time.sleep(1)
+     else: 
+         print(x)
+         time.sleep(1)
+         led2.value = True
+         time.sleep(.2)
+         led2.value = False
+         time.sleep(.2)
+    
