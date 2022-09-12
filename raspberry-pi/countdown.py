4a9e83d22cf848e0862e61f7dd63f2d7d358a70b
nmckee78 <nmckee78@charlottesvilleschools.org>
2022-09-12 13:27:06 -0400

    Commit

--- raspberry-pi/countdown.py
+++ raspberry-pi/countdown.py
@@ -1,10 +1,26 @@
 
 import board
 import time
- 
-for x in range(10,0,-1):
-    if x == 0:
-        print("Liftoff!")
-    else: 
-        print(x)
-        time.sleep(.1)
+import digitalio
+
+led1 = digitalio.DigitalInOut(board.GP13)
+led2 = digitalio.DigitalInOut(board.GP18)
+led1.direction = digitalio.Direction.OUTPUT
+led2.direction = digitalio.Direction.OUTPUT
+button = digitalio.DigitalInOut(board.GP22)
+button.pull = digitalio.Pull.UP
+
+while True:
+    if button.value == False:
+     for x in range(10,-1,-1):
+        if x == 0:
+         print("Liftoff!")
+         led1.value = True
+         time.sleep(1)
+     else: 
+         print(x)
+         time.sleep(1)
+         led2.value = True
+         time.sleep(.2)
+         led2.value = False
+         time.sleep(.2)
